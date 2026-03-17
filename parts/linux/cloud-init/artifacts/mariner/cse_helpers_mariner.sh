7ece80833343c4e58a3154e1cf11e40af7a7b215
Sindhu Karri <lakarri@microsoft.com>
2026-03-17 09:41:19 +0000

    Fix dnf download command to use dnf5-compatible --destdir option

--- parts/linux/cloud-init/artifacts/mariner/cse_helpers_mariner.sh
+++ parts/linux/cloud-init/artifacts/mariner/cse_helpers_mariner.sh
@@ -68,7 +68,7 @@ dnf_download() {
     retries=$1; wait_sleep=$2; timeout=$3; downloadDir=$4; shift && shift && shift && shift
     mkdir -p "${downloadDir}"
     for i in $(seq 1 $retries); do
-        dnf download --resolve --downloaddir="${downloadDir}" "$@" && break || \
+        dnf download --resolve --destdir="${downloadDir}" "$@" && break || \
         if [ $i -eq $retries ]; then
             return 1
         else
